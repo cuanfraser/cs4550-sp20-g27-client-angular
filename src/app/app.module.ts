bb8b086984dfccdc9c68184bc2e44c2ead002b12
Cuan <33520989+cuanfraser@users.noreply.github.com>
2020-04-19 23:11:40 -0400

    Fixed profile component import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
